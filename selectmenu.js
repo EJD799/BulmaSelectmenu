e2e65e81cbdf6f5439ba8af0a0a0544b6fb53c95
EJD799 <108157991+EJD799@users.noreply.github.com>
2026-01-30 14:45:42 +0900

    update

--- selectmenu.js
+++ selectmenu.js
@@ -1,117 +1,116 @@
 // Bulma Selectmenu v1.0.0
 // Made by EJD799
 
-function openSelectmenu(el) {
-    el.classList.add("is-focused");
-    let card = populateMenu(el);
-    positionMenu(card, el);
-}
+let bulmaSelectmenu = {
+    openMenu: function(el) {
+        el.classList.add("is-focused");
+        let card = bulmaSelectmenu.populateMenu(el);
+        bulmaSelectmenu.positionMenu(card, el);
+    },
 
-function attachSelectmenu(el) {
-  el.addEventListener("mousedown", function (e) {
-    e.preventDefault();
-    e.stopPropagation(); // ← CRITICAL
+    attachMenu: function(el) {
+        el.addEventListener("mousedown", function (e) {
+            e.preventDefault();
+            e.stopPropagation(); // ← CRITICAL
 
-    const existing = document.getElementById(`${el.id}_menuCard`);
+            const existing = document.getElementById(`${el.id}_menuCard`);
 
-    if (existing) {
-      existing.remove(); // toggle off
-    } else {
-      openSelectmenu(el); // toggle on
-    }
-  });
-}
+            if (existing) {
+                existing.remove(); // toggle off
+            } else {
+                bulmaSelectmenu.openMenu(el); // toggle on
+            }
+        });
+    },
 
 
-function positionMenu(menu, button) {
-  const rect = button.getBoundingClientRect();
+    positionMenu: function(menu, button) {
+        const rect = button.getBoundingClientRect();
 
-  menu.style.position = "absolute";
-  menu.style.left = `${rect.left + window.scrollX}px`;
-  menu.style.top  = `${rect.bottom + window.scrollY}px`;
-}
+        menu.style.position = "absolute";
+        menu.style.left = `${rect.left + window.scrollX}px`;
+        menu.style.top  = `${rect.bottom + window.scrollY}px`;
+    },
 
-function populateMenu(el) {
-    if (el.getAttribute("id")) {
-        let card = document.createElement("div");
-        card.setAttribute("id", `${el.getAttribute("id")}_menuCard`);
-        card.setAttribute("class", "card selectmenu_menu");
+    populateMenu: function(el) {
+        if (el.getAttribute("id")) {
+            let card = document.createElement("div");
+            card.setAttribute("id", `${el.getAttribute("id")}_menuCard`);
+            card.setAttribute("class", "card selectmenu_menu");
 
-        let items = el.children;
-        for (let i = 0; i < items.length; i++) {
-            let item = items[i];
-            if (item.tagName == "OPTION") {
-                let option = document.createElement("div");
-                option.classList.add("selectmenu_item");
-                if (!item.disabled) {
-                    option.addEventListener("click", function(e) {
-                        selectOption(el, item.value);
-                    });
-                }
-                if (item.disabled) {
-                    option.classList.add("selectmenu_item_disabled");
-                }
-                if (el.value == item.value) {
-                    option.classList.add("selectmenu_item_selected");
-                }
-                option.innerHTML = item.innerHTML;
-                card.appendChild(option);
-            } else if (item.tagName == "OPTGROUP") {
-                if (i > 0) {
-                    //card.appendChild(document.createElement("br"));
-                }
-                let title = document.createElement("span");
-                title.setAttribute("class", "title is-6");
-                title.innerHTML = item.getAttribute("label");
-                card.appendChild(title);
-                let subItems = item.children;
-                for (let j = 0; j < subItems.length; j++) {
-                    let subItem = subItems[j];
+            let items = el.children;
+            for (let i = 0; i < items.length; i++) {
+                let item = items[i];
+                if (item.tagName == "OPTION") {
                     let option = document.createElement("div");
                     option.classList.add("selectmenu_item");
-                    if (!subItem.disabled) {
+                    if (!item.disabled) {
                         option.addEventListener("click", function(e) {
-                            selectOption(el, subItem.value);
+                            bulmaSelectmenu.selectOption(el, item.value);
                         });
                     }
-                    if (subItem.disabled) {
+                    if (item.disabled) {
                         option.classList.add("selectmenu_item_disabled");
                     }
-                    if (el.value == subItem.value) {
+                    if (el.value == item.value) {
                         option.classList.add("selectmenu_item_selected");
                     }
-                    option.innerHTML = subItem.innerHTML;
+                    option.innerHTML = item.innerHTML;
                     card.appendChild(option);
+                } else if (item.tagName == "OPTGROUP") {
+                    let title = document.createElement("span");
+                    title.setAttribute("class", "title is-6");
+                    title.innerHTML = item.getAttribute("label");
+                    card.appendChild(title);
+                    let subItems = item.children;
+                    for (let j = 0; j < subItems.length; j++) {
+                        let subItem = subItems[j];
+                        let option = document.createElement("div");
+                        option.classList.add("selectmenu_item");
+                        if (!subItem.disabled) {
+                            option.addEventListener("click", function(e) {
+                                bulmaSelectmenu.selectOption(el, subItem.value);
+                            });
+                        }
+                        if (subItem.disabled) {
+                            option.classList.add("selectmenu_item_disabled");
+                        }
+                        if (el.value == subItem.value) {
+                            option.classList.add("selectmenu_item_selected");
+                        }
+                        option.innerHTML = subItem.innerHTML;
+                        card.appendChild(option);
+                    }
                 }
             }
+
+            card.addEventListener("mousedown", e => {
+                e.stopPropagation();
+            });
+
+            document.body.appendChild(card);
+            return card;
+        } else {
+            console.warn("[Bulma Selectmenu] The select element does not have an ID. IDs are required for Bulma Selectmenu to work.");
         }
+    },
 
-        card.addEventListener("mousedown", e => {
-            e.stopPropagation();
-        });
+    closeAllMenus: function() {
+        document.querySelectorAll(".selectmenu_menu").forEach(m => m.remove());
+        document.querySelectorAll(".select select.is-focused").forEach(m => m.classList.remove("is-focused"));
+    },
 
-        document.body.appendChild(card);
-        return card;
-    } else {
-        console.warn("[Bulma Selectmenu] The select element does not have an ID. IDs are required for Bulma Selectmenu to work.");
+    selectOption: function(menu, selection) {
+        menu.value = selection;
+        bulmaSelectmenu.closeAllMenus();
     }
-}
-
-function closeAllSelectmenus() {
-  document.querySelectorAll(".selectmenu_menu").forEach(m => m.remove());
-  document.querySelectorAll(".select select.is-focused").forEach(m => m.classList.remove("is-focused"));
 }
 
 document.addEventListener("mousedown", (e) => {
-  if (!e.target.closest(".selectmenu_menu") &&
-      !e.target.closest(".selectmenu_button")) {
-    closeAllSelectmenus();
-  }
+    if (!e.target.closest(".selectmenu_menu") &&
+        !e.target.closest(".selectmenu_button")) {
+        bulmaSelectmenu.closeAllMenus();
+    }
 });
 
-function selectOption(menu, selection) {
-    menu.value = selection;
-    closeAllSelectmenus();
-}
-
-attachSelectmenu(menu);
+bulmaSelectmenu.attachMenu(menu);
